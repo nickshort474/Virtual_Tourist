6f04888f5ff8778e03de88e32d2c4cf2c83c42d3
Nick Short <nickshorty2010@yahoo.co.uk>
2015-11-22 16:09:38 +0000

    First commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Nick Short on 22/11/2015.
+//  Copyright © 2015 Nick Short. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
